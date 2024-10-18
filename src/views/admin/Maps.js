3e7cba079ab8144ad9279d75c1af5043b2a17fd7
Rahul <rrai06125@gmail.com>
2024-10-18 08:43:35 -0700

    Authentication Flow Added Successfully

--- src/views/admin/Maps.js
+++ src/views/admin/Maps.js
@@ -6,7 +6,7 @@ export default function Maps() {
       <div className="flex flex-wrap">
         <div className="w-full px-4">
           <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded items-center justify-center" style={{minHeight:"54vh"}}>
-            <h3>Maps</h3>
+            <h3> Maps </h3>
           </div>
         </div>
       </div>
